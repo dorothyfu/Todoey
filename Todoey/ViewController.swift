752cac1c0f2211ae47a2e84ab26d2174471bc2cc
dorothyfu <dhkfu@edu.uwaterloo.ca>
2020-02-04 16:25:05 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Dorothy Fu on 2020-02-04.
+//  Copyright © 2020 Dorothy Fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
