d56d15dfb76bdfcdfe19290f60b5ec3d916f1693
dorothyfu <dhkfu@edu.uwaterloo.ca>
2020-02-04 19:46:50 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dorothy Fu on 2020-02-04.
+//  Copyright © 2020 Dorothy Fu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
